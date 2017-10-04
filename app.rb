a418a9e2c554bb8ce8b9f344a1f641dd97d05f41
Sam Rae <samuelrae@gmail.com>
2017-10-05 00:00:46 +0100

    initial commit

---
+++ app.rb
@@ -0,0 +1,8 @@
+# app.rb
+
+require 'sinatra'
+require 'sinatra/activerecord'
+require './environments'
+
+class Post < ActiveRecord::Base
+end
---
+++ config.ru
@@ -0,0 +1,5 @@
+# config.ru
+# config file that heroku looks for
+
+require './app'
+run Sinatra::Application
