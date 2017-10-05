0f201be71ac01ce1394cbf118b1fb025352c0046
Sam Rae <samuelrae@gmail.com>
2017-10-06 00:14:59 +0100

    got create and read working

--- app.rb
+++ app.rb
@@ -2,7 +2,55 @@
 
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'json'
 require './environments'
+require 'multi_json'
 
 class Post < ActiveRecord::Base
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :body, presence: true
+end
+
+before do
+ content_type :json
+end
+
+helpers do
+  def json( dataset )
+    if !dataset #.empty?
+      return no_data!
+    else
+      JSON.pretty_generate(JSON.load(dataset.to_json)) + "\n"
+    end
+  end
+  def no_data!
+    status 204
+    #json :message => “no data”
+  end
+end
+
+get "/" do
+  @posts = Post.order("created_at DESC")
+  json @posts
+end
+
+get "/posts/:id" do
+ @post = Post.find(params[:id])
+ json @post
+end
+
+post "/posts/:id" do
+ @post = Post.find(params[:id])
+ json @post
+end
+
+#Create
+post "/posts" do
+ new_post = MultiJson.load(request.body.read)
+ @post = Post.new( new_post )
+ if @post.save
+  json @post
+ else
+  json @post.errors
+ end
 end
